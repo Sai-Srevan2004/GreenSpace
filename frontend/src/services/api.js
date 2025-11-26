ebfea6124f34d1ab3ea3830d24984ca2672df9b0
Sai-Srevan2004 <22bd1a057l@gmail.com>
2025-11-26 14:36:11 +0530

    added deployed backend url in frontend api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+//http://localhost:5000/api
+
+const API_BASE_URL = 'https://greenspace-iynp.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
