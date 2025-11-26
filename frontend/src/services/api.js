1a131ec5f2b1b89e79e0b3163bbc59dcd5652888
Sai-Srevan2004 <22bd1a057l@gmail.com>
2025-11-26 14:51:09 +0530

    deployed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //http://localhost:5000/api
 
-const API_BASE_URL = 'https://greenspace-iynp.onrender.com';
+const API_BASE_URL = 'https://greenspace-iynp.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
